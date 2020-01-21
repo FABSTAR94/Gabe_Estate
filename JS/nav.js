99a4e638640e667418d5f346ae7f251e6bf9d65b
Faviola Caballero <fabycg18@gmail.com>
2020-01-20 18:24:45 -0800

    added mobile view on navbar

--- JS/nav.js
+++ JS/nav.js
@@ -1,14 +1,39 @@
 const nav = document.getElementById('nav');
+const navbarWrapper = document.getElementById('navbar-wrapper');
+const mobileNavbar = navbarWrapper.childNodes[1];
+const regularNavbar = navbarWrapper.childNodes[4];
 
-  window.onscroll = function() {
-    if (window.pageYOffset > 550) {
-      nav.style.background = "rgba(227, 23, 10, 0.8)";
-      nav.classList.add('navbarbottom');
-      nav.classList.remove('navbar');
-    } else {
-      nav.style.background = "rgba(0, 0, 0, 0)";
-      nav.classList.add('navbar');
-      nav.classList.remove('navbarbottom');
-    }
-
+window.onscroll = function() {
+  if (window.pageYOffset > 550) {
+    nav.style.background = "rgba(227, 23, 10, 0.8)";
+    nav.classList.add('navbarbottom');
+    nav.classList.remove('navbar');
+  } else {
+    nav.style.background = "rgba(0, 0, 0, 0)";
+    nav.classList.add('navbar');
+    nav.classList.remove('navbarbottom');
+  }
 }
+
+// <nav class="navbar navbar-dark bg-dark">
+//     <button class="navbar-toggler"
+//     type="button" data-toggle="collapse"
+//     data-target="#navbarToggleExternalContent"
+//     aria-controls="navbarToggleExternalContent"
+//     aria-expanded="false"
+//     aria-label="Toggle navigation">
+//       <span class="navbar-toggler-icon"></span>
+//     </button>
+//   </nav>
+
+window.onload = function() {
+  if (window.outerWidth <= 425) {
+    navbarWrapper.removeChild(navbarWrapper.childNodes[3]);
+    navbarWrapper.insertBefore(mobileNavbar, navbarWrapper.childNodes[1])
+  } else {
+    navbarWrapper.removeChild(navbarWrapper.childNodes[1]);
+  }
+}
+
+
+
